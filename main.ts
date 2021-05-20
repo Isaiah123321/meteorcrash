cb1252c5fb716eedda2a4165feed43f2af92d5ed
Isaiah123321 <84051071+Isaiah123321@users.noreply.github.com>
2021-05-20 15:47:57 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,52 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+sprites.onCreated(SpriteKind.Projectile, function (sprite) {
+    if (info.score() == 100) {
+        game.over(true)
+    }
+    info.changeScoreBy(1)
+})
+let projectile: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . 3 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . 3 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.y = 100
+controller.moveSprite(mySprite, 100, 0)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(200, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . b b b b b b . . . . . 
+        . . . b b 9 9 9 9 9 9 b b . . . 
+        . . b b 9 9 9 9 9 9 9 9 b b . . 
+        . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+        . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+        b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+        b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+        b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+        b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+        b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+        b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+        . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+        . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+        . . b d 5 d 3 3 3 3 5 5 b b . . 
+        . . . b b 5 5 5 5 5 5 b b . . . 
+        . . . . . b b b b b b . . . . . 
+        `, randint(0, 100), 50)
+    projectile.x = randint(0, 10)
+})
